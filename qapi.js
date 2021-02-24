6f2b319be306ddc000274c89f45943c1b90c787b
Proja4242 <75467505+Proja4242@users.noreply.github.com>
2021-02-24 11:19:18 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6035e6e3752fdd001f933fce",
+				"6035e6e3752fdd001f933fd0"
+			]
+		},
+		"6035e6e3752fdd001f933fce": {
+			"id": "6035e6e3752fdd001f933fce",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6035e6e3752fdd001f933fd0": {
+			"id": "6035e6e3752fdd001f933fd0",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
